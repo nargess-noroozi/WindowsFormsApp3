6b32fa01370958d70a837ddd4997e5a5c69d7df9
nargess-noroozi <nnarges283@gmail.com>
2023-11-13 09:09:14 +0000

    Initial commit

---
+++ WindowsFormsApp3/Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = textBox1.Text;
+                string family = textBox2.Text;
+                string tel = textBox3.Text;
+                string line = name + "," + family + "," + tel + "\n";
+                File.AppendAllText("sample.csv", line);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp3/Form3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            string[] lines = File.ReadAllLines("sample.csv");
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                listBox1.Items.Add(lines[i]);
+            }
+        }
+    }
+}
